79529099d6439198c8b6af0844901ae82c4c15f5
V1tor100c <159326966+V1tor100c@users.noreply.github.com>
2025-04-11 19:26:57 -0300

    Atualizar o laranca.cpp

--- laranca.cpp
+++ laranca.cpp
@@ -11,7 +11,10 @@ void Laracna::localizaLaracna(){
     localiza.posiçãoAtual();
 }
 
-void Laracna::movaAte(){
+void Laracna::movaAte(int x, int y, int z){
+   movex = x;
+   movey = y;
+   movex = z;
    moveLaracna();
    localizaLaracna();
 }
